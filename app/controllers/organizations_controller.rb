2bff5ca4b6216878235acc3f65049148d5caade4
UmarFBajwa <umarbajwa786@gmail.com>
2016-06-25 17:05:06 -0700

    Add routes for Organizations, update skills/cause validations for uniqueness, and add delete_all for seed file

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,32 @@
 class OrganizationsController < ApplicationController
+	def index
+		organizations = Organization.all
+		render json: organizations
+	end
+
+	def create
+		neworganization = Organization.new(organization_params)
+		if organization.save
+			render json: {organization: neworganization}
+		else
+			render json: {error: "Unable to create organization"}
+		end
+	end
+
+	def show
+		organization = Organization.find_by(id: params[:id])
+		causes = Cause.where(causable_id: params[:id], causable_type: 'Organization')
+		render json: {organization: organization, causes: causes}
+	end
+
+
+	# Do we need this?
+	# def update
+	# 	organization = Organization.find_by(id: params[:id])
+	# end
+
+	private
+	def org_params
+		params.permit(:name, :email, :zip, :mission_statement, :phone_number, :website_url, :password_digest, :image_url)
+	end
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,4 +1,5 @@
 class Skill < ActiveRecord::Base
   belongs_to :skillable, polymorphic: true
   validates_presence_of :name
+  validates_uniqueness_of :name, :scope => [:skillable, :skillable_type]
 end
