e510f7c23c7dddcd9572e131fa30f4895fdd1fab
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-06-25 20:35:39 -0700

    Add resources for causes and events_users.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   resources :events
   resources :skills
+  resources :causes
+  resources :events_users
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
