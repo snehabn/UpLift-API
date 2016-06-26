94e4790bf2e8d68191f241a4c07f7d318cae0ec4
UmarFBajwa <umarbajwa786@gmail.com>
2016-06-25 21:11:45 -0700

    Add root for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :users
   resources :organizations
   resources :skills
+
+  root 'users#index'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
