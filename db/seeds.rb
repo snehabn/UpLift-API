8f8e2b04241ffca7fb15cb6815cd52670a62777f
UmarFBajwa <umarbajwa786@gmail.com>
2016-06-30 20:00:31 -0700

    Update Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ EventsUser.delete_all
 Skill.delete_all
 Cause.delete_all
 
+# 1
 Organization.create(name: "American Red Cross",
                     email: "contact@redcross.org",
                     zip: 94102,
@@ -21,6 +22,7 @@ Organization.create(name: "American Red Cross",
                     password: "12345",
                     image_url: "https://wwwp.oakland.edu/Assets/upload/images/CSA/redcross.jpg")
 
+# 2
 Organization.create(name: "Youth Speaks",
                     email: "info@youthspeaks.org",
                     zip: 94103,
@@ -30,6 +32,7 @@ Organization.create(name: "Youth Speaks",
                     password: "12345",
                     image_url: "http://youthspeaks.org/wp-content/uploads/2015/03/NewYouthSpeaksLogo-blk-org.png")
 
+# 3
 Organization.create(name: "SF Marin Food Bank",
                     email: "cjang@sfmfoodbank.org",
                     zip: 94107,
@@ -39,15 +42,16 @@ Organization.create(name: "SF Marin Food Bank",
                     password: "12345",
                     image_url: "http://www.sfmfoodbank.org/sites/default/files/images/news/sfmfblogolg.jpg")
 
+# 4
 Organization.create(name: "Homeless Prenatal Program",
                     email: "info@homelessprenatal.org",
                     zip: 94110,
-                    mission_statement: "n partnership with our families, break the cycle of childhood poverty.",
+                    mission_statement: "In partnership with our families, break the cycle of childhood poverty.",
                     phone_number: "415-546-6756, ext. 381",
                     website_url: "http://www.homelessprenatal.org/",
                     password: "12345",
                     image_url: "http://www.homelessprenatal.org/content/uploads/HPP3colorlogo3.png")
-
+# 5
 Organization.create(name: "Meals on Wheels",
                     email: "info@mowsf.org",
                     zip: 94124,
@@ -57,6 +61,7 @@ Organization.create(name: "Meals on Wheels",
                     password: "12345",
                     image_url: "http://www.mowsf.org/wp-content/uploads/2013/04/MOW_Logo_Large_horizontallayout-noback1.png")
 
+# 6
 Organization.create(name: "Boys & Girls Clubs of San Francisco",
                     email: "elooper@kidsclub.org",
                     zip: 94110,
@@ -66,6 +71,7 @@ Organization.create(name: "Boys & Girls Clubs of San Francisco",
                     password: "12345",
                     image_url: "http://media1.fdncms.com/sfweekly/imager/fake-boys-and-girls-club-volunteers-scamming/u/original/3083437/partner-boysandgirlsclub.png")
 
+# 7
 Organization.create(name: "Alameda County Community Food Bank",
                     email: "info@accfb.org",
                     zip: 94621,
@@ -74,6 +80,8 @@ Organization.create(name: "Alameda County Community Food Bank",
                     website_url: "http://www.accfb.org",
                     password: "12345",
                     image_url: "http://cdn9.staztic.com/app/a/2121/2121140/alameda-county-food-bank-637404-l-140x140.png")
+
+# 8
 Organization.create(name: "Habitat for Humanity Greater San Francisco",
                     email: "info@habitatgsf.org",
                     zip: 94111,
@@ -82,6 +90,8 @@ Organization.create(name: "Habitat for Humanity Greater San Francisco",
                     website_url: "https://www.habitatgsf.org",
                     password: "12345",
                     image_url: "https://farm8.staticflickr.com/7421/buddyicons/76411288@N03_l.jpg?1391809638")
+
+# 9
 Organization.create(name: "California State Parks Foundation (CSPF)",
                     email: "info@calparks.org",
                     zip: 94111,
@@ -90,6 +100,8 @@ Organization.create(name: "California State Parks Foundation (CSPF)",
                     website_url: "http://www.calparks.org",
                     password: "12345",
                     image_url: "http://www.parks.ca.gov/pages/24510/images/state_parks_foundation_logo.png")
+
+# 10
 Organization.create(name: "Second Harvest Food Bank, Santa Cruz County",
                     email: "mary@thefoodbank.org",
                     zip: 95076,
@@ -98,6 +110,8 @@ Organization.create(name: "Second Harvest Food Bank, Santa Cruz County",
                     website_url: "http://www.thefoodbank.org",
                     password: "12345",
                     image_url: "http://www.donateacar.com/images/SecondHarvestFoodBank.png")
+
+# 11
 Organization.create(name: "Brian Chung's Center for Kids Who Can't Code  Good and Wanna Learn To Implement Other API's Too",
                     email: "brianjaychung@gmail.com",
                     zip: 95076,
@@ -110,14 +124,14 @@ Organization.create(name: "Brian Chung's Center for Kids Who Can't Code  Good an
 Event.create(name: "Blood Drive", start_date: "2016-07-15", end_date: "2016-07-18", organization_id: 1, street: "140 Gregory Ln #120", city: "Pleasant Hill", state: "CA", zip: 94523, volunteers_needed: 50, lat: 37.9541327800932, lng: -122.076140163367)
 Event.create(name: "Youth Poetry Slam", start_date: "2016-07-12", end_date: "2016-07-16", organization_id: 2, street: "1663 Mission St.", city: "San Francisco", state: "CA", zip: 94103, volunteers_needed: 200, lat: 37.7731518004931, lng: -122.411163768557)
 Event.create(name: "Park Champions Program", start_date: "2016-07-01", end_date: "Ongoing", organization_id: 7, street: "50 Francisco Street", city: "San Francisco", state: "CA", zip: 94133, volunteers_needed: 500, lat: 37.806357, lng: -122.406076)
-Event.create(name: "Park Champions Program at Candlestick Point State Recreation Area", start_date: "2016-07-01", end_date: "Ongoing", organization_id: 7, street: "1150 Carroll Avenue", city: "San Francisco", state: "CA", zip: 94124, volunteers_needed: 500, lat: 37.720865, lng: -122.385062)
-Event.create(name: "Kids Now Program - Second Harvest Food Bank", start_date: "2016-07-05", end_date: "Ongoing", organization_id: 8, street: "1720 El Camino Real, Suite 10", city: "Burlingame", state: "CA", zip: 94010, volunteers_needed: 500, lat: 37.593731, lng: -122.381107)
+Event.create(name: "Park Champions Program at Candlestick Point State Recreation Area", start_date: "2016-07-01", end_date: "Ongoing", organization_id: 9, street: "1150 Carroll Avenue", city: "San Francisco", state: "CA", zip: 94124, volunteers_needed: 500, lat: 37.720865, lng: -122.385062)
+Event.create(name: "Kids Now Program - Second Harvest Food Bank", start_date: "2016-07-05", end_date: "Ongoing", organization_id: 10, street: "1720 El Camino Real, Suite 10", city: "Burlingame", state: "CA", zip: 94010, volunteers_needed: 500, lat: 37.593731, lng: -122.381107)
 Event.create(name: "Summer Office", start_date: "2016-08-01", end_date: "2016-09-30", organization_id: 9, street: "500 Washington St. Suite 250", city: "San Francisco", state: "CA", zip: 94111, volunteers_needed: 3, lat: 37.796210, lng: -122.402130)
-Event.create(name: "Volunteer Photographer", start_date: "2016-07-05", end_date: "2016-07-12", organization_id: 9, street: "1 Capitol Ave.", city: "San Francisco", state: "CA", zip: 94112, volunteers_needed: 1, lat: 37.711121, lng:  -122.459063)
-Event.create(name: "Construction Crew Volunteer", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 9, street: "1 Capitol Ave.", city: "San Francisco", state: "CA", zip: 94112, volunteers_needed: 40, lat: 37.711121, lng:  -122.459063)
-Event.create(name: "Fishing with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 10, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
-Event.create(name: "Lifting with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 10, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
-Event.create(name: "Hour of APIs with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 10, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
+Event.create(name: "Volunteer Photographer", start_date: "2016-07-05", end_date: "2016-07-12", organization_id: 8, street: "1 Capitol Ave.", city: "San Francisco", state: "CA", zip: 94112, volunteers_needed: 1, lat: 37.711121, lng:  -122.459063)
+Event.create(name: "Construction Crew Volunteer", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 8, street: "1 Capitol Ave.", city: "San Francisco", state: "CA", zip: 94112, volunteers_needed: 40, lat: 37.711121, lng:  -122.459063)
+Event.create(name: "Fishing with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 11, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
+Event.create(name: "Lifting with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 11, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
+Event.create(name: "Hour of APIs with Brian", start_date: "2016-07-05", end_date: "2016-09-30", organization_id: 11, street: "633 Folsom", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 100, lat: 37.784823, lng:  -122.397204)
 Event.create(name: "CalFresh Outreach Volunteer", start_date: "2016-06-26", end_date: "Ongoing", organization_id: 3, street: "900 Pennsylvania Ave", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 30, lat: 37.7731518004931, lng: -122.411163768557)
 Event.create(name: "Volunteer at the Food Bank!", start_date: "2016-06-26", end_date: "Ongoing", organization_id: 3, street: "900 Pennsylvania Ave", city: "San Francisco", state: "CA", zip: 94107, volunteers_needed: 55, lat: 37.7731518004931, lng: -122.411163768557)
 Event.create(name: "Driver Opportunities: Deliver nutritious groceries to homebound seniors!", start_date: "2016-06-26", end_date: "Ongoing", organization_id: 5, street: "1375 Fairfax Ave", city: "San Francisco", state: "CA", zip: 94124, volunteers_needed: 55, lat: 37.7731518004931, lng: -122.411163768557)
@@ -135,15 +149,18 @@ EventsUser.create(user_id: 1, event_id: 2)
 EventsUser.create(user_id: 2, event_id: 1)
 EventsUser.create(user_id: 2, event_id: 2)
 
-Skill.create(name: 'programming', skillable_id: rand(1..2) , skillable_type: ['User', 'Event'].sample)
+Skill.create(name: 'programming', skillable_id: 11, skillable_type: ['Event'].sample)
+Skill.create(name: 'programming', skillable_id: 1, skillable_type: ['User'].sample)
+Skill.create(name: 'programming', skillable_id: 2, skillable_type: ['User'].sample)
 Skill.create(name: 'business development', skillable_id: rand(1..2) , skillable_type: ['User', 'Event'].sample)
-Skill.create(name: 'gardening',  skillable_id: rand(1..2) , skillable_type: ['User', 'Event'].sample)
-Skill.create(name: 'teaching',  skillable_id: rand(1..2) , skillable_type: ['User', 'Event'].sample)
+Skill.create(name: 'gardening',  skillable_id: 8, skillable_type: ['Event'].sample)
+Skill.create(name: 'teaching',  skillable_id: 6, skillable_type: ['Event'].sample)
+Skill.create(name: 'teaching',  skillable_id: 2, skillable_type: ['User'].sample)
 Skill.create(name: 'fishing',  skillable_id: 10, skillable_type: ['Event'].sample)
 Skill.create(name: 'dancing',  skillable_id: 18, skillable_type: ['Event'].sample)
 Skill.create(name: 'driving',  skillable_id: 14, skillable_type: ['Event'].sample)
-Skill.create(name: 'photography',  skillable_id: 9, skillable_type: ['Event'].sample)
-Skill.create(name: 'construction',  skillable_id: 9, skillable_type: ['Event'].sample)
+Skill.create(name: 'photography',  skillable_id: 7, skillable_type: ['Event'].sample)
+Skill.create(name: 'construction',  skillable_id: 8, skillable_type: ['Event'].sample)
 
 
 
